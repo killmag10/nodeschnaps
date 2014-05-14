f4ea3112669da748d962ae55cb4b8d1f30479450
Lars Dietrich <lars@dietrich-hosting.de>
2014-05-14 23:44:45 +0200

    [core] Add console module support.

---
+++ test/lib/nodeschnaps/node/console.js
@@ -0,0 +1,18 @@
+var should = require('should');
+
+exports.test = function(){
+    console.log('console.log',1,2,{ 'test' : 'abc' });
+    console.info('console.info',1,2,{ 'test' : 'abc' });
+    console.error('console.error',1,2,{ 'test' : 'abc' });
+    console.warn('console.warn',1,2,{ 'test' : 'abc' });
+    console.dir({ 'test' : 'abc' });
+    console.time('test');
+    console.timeEnd('test');
+    console.trace('test trace');
+    console.assert(true,'test');
+    try{
+        console.assert(false,'test');
+    } catch(e) {
+        console.log(e);
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 var should = require('should');
 
 tests = [
-    'lib/nodeschnaps/node/process.js'
+    'lib/nodeschnaps/node/process.js',
+    'lib/nodeschnaps/node/console.js'
 ]
 
 tests.forEach(function(item){
+    console.log('##### TEST: ' + item);
     var test = require('./' + item);
     test.test();
 });
