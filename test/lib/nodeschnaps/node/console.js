36cba83f8577ea604864eea2e1432ca2902015df
Lars Dietrich <lars@dietrich-hosting.de>
2014-05-19 01:32:42 +0200

    [testing] Add testing of console.

--- test/lib/nodeschnaps/node/console.js
+++ test/lib/nodeschnaps/node/console.js
@@ -1,1 +1,62 @@
+QUnit.test( "console.log", function() {
+    QUnit.ok(
+        console.log instanceof Function,
+        "Should be an instance of Function."
+    );
+});
 
+QUnit.test( "console.info", function() {
+    QUnit.ok(
+        console.info instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.error", function() {
+    QUnit.ok(
+        console.error instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.warn", function() {
+    QUnit.ok(
+        console.warn instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.dir", function() {
+    QUnit.ok(
+        console.dir instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.time", function() {
+    QUnit.ok(
+        console.time instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.timeEnd", function() {
+    QUnit.ok(
+        console.timeEnd instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.trace", function() {
+    QUnit.ok(
+        console.trace instanceof Function,
+        "Should be an instance of Function."
+    );
+});
+
+QUnit.test( "console.assert", function() {
+    QUnit.ok(
+        console.assert instanceof Function,
+        "Should be an instance of Function."
+    );
+});
