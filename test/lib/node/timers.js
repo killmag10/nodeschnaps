9570f4e27d697f90f1d340618b485c51b951d0a3
Lars Dietrich <lars@dietrich-hosting.de>
2014-08-29 23:01:49 +0200

    [core][timer] Correct timer functions.

--- test/lib/node/timers.js
+++ test/lib/node/timers.js
@@ -1,9 +1,14 @@
 QUnit.asyncTest( "timers.setTimeout", function() {
-    expect( 2 );
+    expect( 3 );
 
     QUnit.ok(
         require('timers').setTimeout instanceof Function,
         "Should be an instance of Function."
+    );
+
+    QUnit.ok(
+        setTimeout instanceof Function,
+        "Should be global."
     );
 
     require('timers').setTimeout(function() {
@@ -18,6 +23,11 @@ QUnit.test( "timers.clearTimeout", function() {
         "Should be an instance of Function."
     );
 
+    QUnit.ok(
+        clearTimeout instanceof Function,
+        "Should be global."
+    );
+
     var timer = require('timers').setTimeout(function() {}, 60000);
     QUnit.ok(
         require('timers').clearTimeout(timer) === undefined,
@@ -26,11 +36,16 @@ QUnit.test( "timers.clearTimeout", function() {
 });
 
 QUnit.asyncTest( "timers.setInterval", function() {
-    expect( 3 );
+    expect( 4 );
 
     QUnit.ok(
         require('timers').setInterval instanceof Function,
         "Should be an instance of Function."
+    );
+
+    QUnit.ok(
+        setInterval instanceof Function,
+        "Should be global."
     );
 
     var first = true;
@@ -51,6 +66,11 @@ QUnit.test( "timers.clearInterval", function() {
         "Should be an instance of Function."
     );
 
+    QUnit.ok(
+        clearInterval instanceof Function,
+        "Should be global."
+    );
+
     var timer = require('timers').setInterval(function() {}, 60000);
     QUnit.ok(
         require('timers').clearInterval(timer) === undefined,
@@ -59,11 +79,16 @@ QUnit.test( "timers.clearInterval", function() {
 });
 
 QUnit.asyncTest( "timers.setImmediate", function() {
-    expect( 11 );
+    expect( 12 );
 
     QUnit.ok(
         require('timers').setImmediate instanceof Function,
         "Should be an instance of Function."
+    );
+
+    QUnit.ok(
+        setImmediate instanceof Function,
+        "Should be global."
     );
 
     for (var i=1; i<10; i++){
@@ -83,9 +108,47 @@ QUnit.test( "timers.clearImmediate", function() {
         "Should be an instance of Function."
     );
 
+    QUnit.ok(
+        clearImmediate instanceof Function,
+        "Should be global."
+    );
+
     var timer = require('timers').setImmediate(function() {});
     QUnit.ok(
         require('timers').clearImmediate(timer) === undefined,
+        "Should be undefined and throws no error."
+    );
+});
+
+QUnit.test( "timers.ref/unref", function() {
+
+    var timer = require('timers').setTimeout(function() {
+        ok( true, "Timeout callback should be called." );
+        start();
+    }, 60000);
+
+    QUnit.ok(
+        timer.ref instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    QUnit.ok(
+        timer.unref instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    QUnit.ok(
+        timer.unref(timer) === undefined,
+        "Should be undefined and throws no error."
+    );
+
+    QUnit.ok(
+        timer.ref(timer) === undefined,
+        "Should be undefined and throws no error."
+    );
+
+    QUnit.ok(
+        require('timers').clearTimeout(timer) === undefined,
         "Should be undefined and throws no error."
     );
 });
