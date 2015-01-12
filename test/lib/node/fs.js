71e4cf581be4c7f1c2ae013a3a34ef9647e60d65
Lars Dietrich <lars@dietrich-hosting.de>
2015-01-12 09:50:25 +0100

    [#2][core][fs] Update fs binding methods

--- test/lib/node/fs.js
+++ test/lib/node/fs.js
@@ -134,6 +134,23 @@ QUnit.test( "fs.fstatSync", function() {
     require('fs').closeSync(fd);
 });
 
+QUnit.test( "fs.fsyncSync", function() {
+    QUnit.ok(
+        require('fs').fstat instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    var fd = require('fs').openSync(
+        testfile,
+        'r'
+    )
+
+    var result = require('fs').fsyncSync(fd);
+    QUnit.ok(result === undefined, "Should return nothing.");
+
+    require('fs').closeSync(fd);
+});
+
 QUnit.asyncTest( "fs.readFile", function() {
     expect( 3 );
 
@@ -166,6 +183,194 @@ QUnit.test( "fs.readFileSync", function() {
         data.toString() === testfileContent,
         "Content should be correct."
     );
+});
+
+
+QUnit.asyncTest( "fs.writeFile", function() {
+    expect( 2 );
+
+    QUnit.ok(
+        require('fs').writeFile instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    require('fs').writeFile(
+        testfile_write,
+        function(err) {
+            if (err) throw err;
+
+            QUnit.ok(
+                true,
+                "Callback should be called."
+            );
+
+            start();
+        }
+    );
+});
+
+QUnit.test( "fs.writeFileSync", function() {
+    QUnit.ok(
+        require('fs').writeFileSync instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    var result = require('fs').writeFileSync(testfile_write, '123\nabc');
+    QUnit.ok(result === undefined, "Should return nothing.");
+});
+
+QUnit.asyncTest( "fs.unlink", function() {
+    expect( 2 );
+
+    require('fs').writeFileSync(testfile_write, 'test unlink');
+
+    QUnit.ok(
+        require('fs').unlink instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    require('fs').unlink(
+        testfile_write,
+        function(err) {
+            if (err) throw err;
+
+            QUnit.ok(
+                true,
+                "Callback should be called."
+            );
+
+            start();
+        }
+    );
+});
+
+QUnit.test( "fs.unlinkSync", function() {
+    require('fs').writeFileSync(testfile_write, 'test unlink');
+
+    QUnit.ok(
+        require('fs').unlinkSync instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    var result = require('fs').unlinkSync(testfile_write);
+    QUnit.ok(result === undefined, "Should return nothing.");
+});
+
+QUnit.asyncTest( "fs.mkdir", function() {
+    expect( 2 );
+
+    QUnit.ok(
+        require('fs').mkdir instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    require('fs').mkdir(
+        testfile_write,
+        function(err) {
+            if (err) throw err;
+
+            QUnit.ok(
+                true,
+                "Callback should be called."
+            );
+
+            require('fs').rmdirSync(testfile_write);
+
+            start();
+        }
+    );
+});
+
+QUnit.test( "fs.mkdirSync", function() {
+    QUnit.ok(
+        require('fs').mkdirSync instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    var result = require('fs').mkdirSync(testfile_write);
+    QUnit.ok(result === undefined, "Should return nothing.");
+
+    require('fs').rmdirSync(testfile_write);
+});
+
+QUnit.asyncTest( "fs.rmdir", function() {
+    expect( 2 );
+
+    require('fs').mkdirSync(testfile_write);
+
+    QUnit.ok(
+        require('fs').rmdir instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    require('fs').rmdir(
+        testfile_write,
+        function(err) {
+            if (err) throw err;
+
+            QUnit.ok(
+                true,
+                "Callback should be called."
+            );
+
+            start();
+        }
+    );
+});
+
+QUnit.test( "fs.rmdirSync", function() {
+    require('fs').mkdirSync(testfile_write);
+
+    QUnit.ok(
+        require('fs').rmdirSync instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    var result = require('fs').rmdirSync(testfile_write);
+    QUnit.ok(result === undefined, "Should return nothing.");
+});
+
+QUnit.asyncTest( "fs.rename", function() {
+    expect( 2 );
+
+    require('fs').writeFileSync(testfile_write, 'test unlink');
+
+    QUnit.ok(
+        require('fs').rename instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    require('fs').rename(
+        testfile_write,
+        testfile_write + '.renamed',
+        function(err) {
+            if (err) throw err;
+
+            QUnit.ok(
+                true,
+                "Callback should be called."
+            );
+
+            require('fs').unlinkSync(testfile_write + '.renamed');
+            start();
+        }
+    );
+});
+
+QUnit.test( "fs.renameSync", function() {
+    require('fs').writeFileSync(testfile_write, 'test unlink');
+
+    QUnit.ok(
+        require('fs').renameSync instanceof Function,
+        "Should be an instance of Function."
+    );
+
+    var result = require('fs').renameSync(
+        testfile_write, testfile_write + '.renamed'
+    );
+    QUnit.ok(result === undefined, "Should return nothing.");
+
+    require('fs').unlinkSync(testfile_write + '.renamed');
 });
 
 
